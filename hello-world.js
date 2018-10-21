5ba87317de376a8666dfa55934845a077fae1e1e
Jisack <jokjack26172@gmail.com>
2018-10-21 11:53:21 +0700

    add function greedyWithName to js language

--- hello-world.js
+++ hello-world.js
@@ -1,10 +1,14 @@
-'use strict'
+"use strict";
 const greetRate = 50;
-function helloWorld(applause){
-   if(applause > 0){
-      console.log('Hello world!');
-   }
-  console.log('No applause :(');
+function helloWorld(applause) {
+  if (applause > 0) {
+    console.log("Hello world!");
+  }
+  console.log("No applause :(");
 }
 
+function greedyWithName(name) {
+  console.log("Greedy, " + name);
+}
 
+greedyWithName("Jack");
